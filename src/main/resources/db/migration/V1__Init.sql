1e79a7c889e006bd785f3291ed748da568e8dbc7
Sumsan38 <ndanl4647@gmail.com>
2025-04-14 11:52:20 +0900

    chore: Integrate Flyway for database schema menagement

---
+++ src/main/resources/db/migration/V1__Init.sql
@@ -0,0 +1,7 @@
+create table users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    role VARCHAR(50) DEFAULT 'user',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
