98efd4afdadfd0c39dc4cc5a8c19aeabd90a669e
Sumsan38 <ndanl4647@gmail.com>
2025-04-14 10:56:08 +0900

    feat: Add GlobalExceptionHandler for centrallized error handling

--- src/main/java/com/som/deliveryplatform/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/som/deliveryplatform/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.som.deliveryplatform.global.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        // 유효성 검사 실패시
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Validation faile: " + errorMessage);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Internal Server Error: " + ex.getMessage());
+    }
 }
