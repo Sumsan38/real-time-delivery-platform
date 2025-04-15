c2b3fe3242fc5926cc303f63600983429984fcdd
Sumsan38 <ndanl4647@gmail.com>
2025-04-15 09:35:06 +0900

    feat: Add UserRepository with findByEmail method

---
+++ src/main/java/com/som/deliveryplatform/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.som.deliveryplatform.domain.user.repository;
+
+import com.som.deliveryplatform.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
