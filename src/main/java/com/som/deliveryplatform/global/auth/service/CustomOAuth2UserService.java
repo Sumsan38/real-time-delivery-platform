96ea35041378a59d666c8bd74596a55c10991319
Sumsan38 <ndanl4647@gmail.com>
2025-04-15 10:08:26 +0900

    feat: save or find user after oauth2 login and return UserPrincipal

--- src/main/java/com/som/deliveryplatform/global/auth/service/CustomOAuth2UserService.java
+++ src/main/java/com/som/deliveryplatform/global/auth/service/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.som.deliveryplatform.global.auth.service;
 
+import com.som.deliveryplatform.domain.user.entity.User;
+import com.som.deliveryplatform.domain.user.repository.UserRepository;
 import com.som.deliveryplatform.global.auth.principal.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -14,23 +16,30 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    private final UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//        userRequest.getClientRegistration().getRegistrationId();  // google, naver, kakao
-
+//        userRequest.getClientRegistration().getRegistrationId();  // google, naver, kakao (차후 연동할 경우 분리)
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        // 1. Provider로부터 받은 사용자 정보
+        // 1. Provider(google)로부터 받은 사용자 정보
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        // 2. 필요에 따라 attributes 가공
-        String email = (String) attributes.get("email");    // google 경우
-        String role = "ROLE_USER";  // 기본적으로 일반 사용자
+        // 2. DB에서 사용자 존재 여부 확인
+        // 존재하지 않다면 새로 회원가입 시킨다
+        String email = (String) attributes.get("email");    // google 경우 email을 그냥 꺼낼 수 있다
+        User findUser = userRepository.findByEmail(email)
+                .orElseGet(() -> userRepository.save(User.builder()
+                        .email(email)
+                        .role("ROLE_USER")
+                        .build()));
 
         // 3. 서버 내부 표준 사용자(UserPrincipal)로 변환
         return UserPrincipal.builder()
-                .email(email)
-                .role(role)
+                .id(findUser.getId())
+                .email(findUser.getEmail())
+                .role(findUser.getRole())
                 .attributes(attributes)
                 .build();
     }
