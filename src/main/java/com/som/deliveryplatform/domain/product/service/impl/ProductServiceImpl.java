248478ee71a1fb6141dc90a05b45e3bb07a6350d
Sumsan38 <ndanl4647@gmail.com>
2025-04-21 10:57:41 +0900

    feat: implement product findById login without cache

--- src/main/java/com/som/deliveryplatform/domain/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/som/deliveryplatform/domain/product/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.som.deliveryplatform.domain.product.service.impl;
 
 import com.som.deliveryplatform.domain.product.dto.response.ProductResponse;
+import com.som.deliveryplatform.domain.product.entity.Product;
 import com.som.deliveryplatform.domain.product.repository.ProductRepository;
 import com.som.deliveryplatform.domain.product.service.ProductCacheService;
 import com.som.deliveryplatform.domain.product.service.ProductService;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -29,5 +31,12 @@ public class ProductServiceImpl implements ProductService {
         productCacheService.setCachedProductList(products);
 
         return products;
+    }
+
+    @Override
+    public ProductResponse findById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+        return ProductResponse.of(product);
     }
 }
