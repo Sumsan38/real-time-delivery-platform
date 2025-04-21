16c540db3d22aab8309e44b3ff1e536a1122b467
Sumsan38 <ndanl4647@gmail.com>
2025-04-21 14:38:57 +0900

    feat: implement product save API and controller

--- src/main/java/com/som/deliveryplatform/domain/product/controller/ProductController.java
+++ src/main/java/com/som/deliveryplatform/domain/product/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.som.deliveryplatform.domain.product.controller;
 
+import com.som.deliveryplatform.domain.product.dto.request.ProductRequest;
 import com.som.deliveryplatform.domain.product.dto.response.ProductResponse;
 import com.som.deliveryplatform.domain.product.service.ProductService;
 import com.som.deliveryplatform.global.common.ResponseCode;
 import com.som.deliveryplatform.global.common.ResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,12 @@ public class ProductController {
         List<ProductResponse> productResponses = productService.findAll();
 
         return ResponseEntity.ok(ResponseDto.of(ResponseCode.SUCCESS, productResponses));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDto<ProductResponse>> saveProduct(@Valid ProductRequest request) {
+        ProductResponse productResponse = productService.save(request);
+
+        return ResponseEntity.ok(ResponseDto.of(ResponseCode.SUCCESS, productResponse));
     }
 }
