bf448ddc0e6ce2a0c0e47f45fd6dd316ddfef329
Sumsan38 <ndanl4647@gmail.com>
2025-04-21 13:27:19 +0900

    feat: add ProductRequest Dto with validation annotations

---
+++ src/main/java/com/som/deliveryplatform/domain/product/dto/request/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.som.deliveryplatform.domain.product.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRequest(
+        @NotBlank(message = "상품명은 필수입니다.")
+        String name,
+        @NotNull(message = "가격은 필수입니다.")
+        @Min(value = 0, message = "가격은 0 이상이여야합니다.")
+        Integer price,
+        @NotNull(message = "재교는 필수입니다.")
+        @Min(value = 0, message = "재교는 0 이상이여야합니다.")
+        Integer stock) {
+}
