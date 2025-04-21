921afd024e0c3e807e85f77626f19546c055a0f5
Sumsan38 <ndanl4647@gmail.com>
2025-04-21 16:07:00 +0900

    fix: ProductController save parameter annotation

--- src/main/java/com/som/deliveryplatform/domain/product/controller/ProductController.java
+++ src/main/java/com/som/deliveryplatform/domain/product/controller/ProductController.java
@@ -8,10 +8,7 @@ import com.som.deliveryplatform.global.common.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +27,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ResponseDto<ProductResponse>> saveProduct(@Valid ProductRequest request) {
+    public ResponseEntity<ResponseDto<ProductResponse>> saveProduct(@Valid @RequestBody ProductRequest request) {
         ProductResponse productResponse = productService.save(request);
 
         return ResponseEntity.ok(ResponseDto.of(ResponseCode.SUCCESS, productResponse));
