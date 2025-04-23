b01376d251720322438ecc9e7df8c87182fd1a2f
Sumsan38 <ndanl4647@gmail.com>
2025-04-23 09:44:33 +0900

    feat: defined Order request, response dto

--- src/main/java/com/som/deliveryplatform/domain/order/dto/request/OrderRequest.java
+++ src/main/java/com/som/deliveryplatform/domain/order/dto/request/OrderRequest.java
@@ -1,4 +1,23 @@
 package com.som.deliveryplatform.domain.order.dto.request;
 
-public class OrderRequest {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record OrderRequest(
+        @NotNull(message = "사용자 ID는 필수입니다")
+        Long userId,
+
+        @NotNull(message = "주문 상품 목록은 필수입니다.")
+        List<OrderItemRequest> items
+) {
+    public record OrderItemRequest(
+            @NotNull(message = "상품 ID는 필수입니다.")
+            Long productId,
+
+            @Min(value = 1, message = "최소 수량은 1 이상이어야 합니다.")
+            int quantity
+    ) {
+    }
 }
--- src/main/java/com/som/deliveryplatform/domain/order/dto/response/OrderResponse.java
+++ src/main/java/com/som/deliveryplatform/domain/order/dto/response/OrderResponse.java
@@ -1,4 +1,40 @@
 package com.som.deliveryplatform.domain.order.dto.response;
 
-public class OrderResponse {
+import com.som.deliveryplatform.domain.order.entity.Order;
+import com.som.deliveryplatform.domain.order.entity.OrderItem;
+import com.som.deliveryplatform.domain.order.entity.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderResponse(
+        Long orderId,
+        Long userId,
+        LocalDateTime orderedAt,
+        OrderStatus status,
+        List<OrderItemResponse> items
+) {
+
+    public static OrderResponse from(Order order) {
+        return new OrderResponse(
+                order.getId(),
+                order.getUserId(),
+                order.getCreatedAt(),
+                order.getStatus(),
+                order.getOrderItems().stream().map(OrderItemResponse::from).toList()
+        );
+    }
+
+    public record OrderItemResponse(
+            Long productId,
+            int quantity,
+            int price) {
+
+        public static OrderItemResponse from(OrderItem item) {
+            return new OrderItemResponse(
+                    item.getProductId(),
+                    item.getQuantity(),
+                    item.getPrice());
+        }
+    }
 }
